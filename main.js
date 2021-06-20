36a8fd2b4b96dbd473f686890222c8b1adb4a4f7
Pimlada <pimdiamond@gmail.com>
2021-06-20 17:51:07 -0400

    added js code

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var apiResult = JSON.parse(this.responseText);
 
+        //Code Dealing With The API Data Goes Here
+console.log(apiResult);
+        
+//  var city = apiResult.main.name;
+//  console.log(city);
+
+
+    }
+};
 xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=33175,us&appid=6efff70fe1477748e31c17d1c504635f', true);
 xmlhttp.send();
